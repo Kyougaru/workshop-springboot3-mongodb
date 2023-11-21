3032fa3bf29b2c00e28ca6ff6be6ad0399b61bde
Kyougaru <gabrielterock@gmail.com>
2023-11-21 15:38:45 -0300

    User post and delete

--- src/main/java/com/kyougaru/workshopmongo/services/UserService.java
+++ src/main/java/com/kyougaru/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kyougaru.workshopmongo.domain.User;
+import com.kyougaru.workshopmongo.dto.UserDTO;
 import com.kyougaru.workshopmongo.repositories.UserRepository;
 import com.kyougaru.workshopmongo.services.exceptions.ResourceNotFoundException;
 
@@ -23,5 +24,20 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public void delete(String id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException(id);
+		}
+		repository.deleteById(id);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
