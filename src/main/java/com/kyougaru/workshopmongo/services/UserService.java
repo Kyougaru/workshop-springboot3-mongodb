71a4074a1076b87ba58c59dd9bddbebc1e2d53e4
Kyougaru <gabrielterock@gmail.com>
2023-11-21 14:35:26 -0300

    User findById and exception handling

--- src/main/java/com/kyougaru/workshopmongo/services/UserService.java
+++ src/main/java/com/kyougaru/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.kyougaru.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kyougaru.workshopmongo.domain.User;
 import com.kyougaru.workshopmongo.repositories.UserRepository;
+import com.kyougaru.workshopmongo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 }
