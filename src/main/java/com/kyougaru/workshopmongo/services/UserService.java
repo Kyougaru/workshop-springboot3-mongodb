1bde3db9d125f1c9aa8433b6160b3eea441c3d91
Kyougaru <gabrielterock@gmail.com>
2023-11-21 16:04:41 -0300

    User put

--- src/main/java/com/kyougaru/workshopmongo/services/UserService.java
+++ src/main/java/com/kyougaru/workshopmongo/services/UserService.java
@@ -37,6 +37,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		Optional<User> newObj = repository.findById(obj.getId());
+		updateData(newObj.orElseThrow(() -> new ResourceNotFoundException(obj.getId())), obj);
+		return repository.save(newObj.get());
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
