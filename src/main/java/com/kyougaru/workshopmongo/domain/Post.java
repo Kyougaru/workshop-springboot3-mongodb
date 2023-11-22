481673fbce110605aeac99bb19f77bd8a1655005
Kyougaru <gabrielterock@gmail.com>
2023-11-22 13:31:58 -0300

    Adding comments to posts

--- src/main/java/com/kyougaru/workshopmongo/domain/Post.java
+++ src/main/java/com/kyougaru/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.kyougaru.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.kyougaru.workshopmongo.dto.AuthorDTO;
+import com.kyougaru.workshopmongo.dto.CommentDTO;
 
 @Document(collection="post")
 public class Post implements Serializable {
@@ -18,7 +21,10 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
+	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -70,6 +76,10 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
 	}
 
 	@Override
